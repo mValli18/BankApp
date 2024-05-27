94b073ba2aa6951de3b537b48171c780357ade89
unknown <srivalli3845@gmail.com>
2024-05-27 11:32:26 +0530

    changes

--- BankAPP/Models/Account.cs
+++ BankAPP/Models/Account.cs
@@ -7,5 +7,6 @@
         public string AccountHolderName { get; set; }
         public decimal Balance { get; set; }
         public string AccountType { get; set; } 
+        private User user { get; set; }
     }
 }
